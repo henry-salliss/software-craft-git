789a69ab952cc004f6b8832d9531204ce1b51a26
joshuabrookcb <joshuabrook@coldbanana.com>
2023-04-12 15:49:00 +0100

    wow, so pretty!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import BankAccount from './BankAccount';
-import CurrentAccount from './CurrentAccount';
-import SavingsAccount from './SavingsAccount';
+import "./App.css";
+import BankAccount from "./BankAccount";
+import CurrentAccount from "./CurrentAccount";
+import SavingsAccount from "./SavingsAccount";
 
 function App() {
   return (
     <div className="App">
-      <BankAccount type='Normal' customerName='Henry'/>
-      <CurrentAccount customerName='Bob'/>
-      <SavingsAccount type='Savings' customerName='John'/>
+      <div class="grid-container">
+        <div class="grid-item">
+        <BankAccount type="Normal" customerName="Henry" />
+        </div>
+        <div class="grid-item">
+        <CurrentAccount customerName="Bob" />
+        </div>
+        <div class="grid-item">
+        <SavingsAccount type="Savings" customerName="John" />
+        </div>
+      </div>
     </div>
   );
 }
